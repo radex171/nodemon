1c9634205bfa3b98d4e0b654ab6644b1fdecc583
Radoslaw Zaborski <radex171@gmail.com>
2020-06-10 14:17:47 +0200

    add layouts

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info', { layout: 'dark' });
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history');
 });
 
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 })
