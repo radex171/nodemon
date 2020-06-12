d4b5cb3e1491cdca150ecffbe1a564f6a314e895
Radoslaw Zaborski <radex171@gmail.com>
2020-06-12 12:41:02 +0200

    add func send message & file

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+const uploadFile = multer();
 
 const app = express();
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -31,6 +35,20 @@ app.get('/history', (req, res) => {
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { name: req.params.name });
 });
+
+app.post('/contact/send-message', uploadFile.single('image'), (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message && req.file) {
+    res.render('contact', { isSent: true, fileName: req.file.originalname });
+  }
+  else {
+    res.render('contact', { isError: true })
+  }
+
+});
+
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 })
